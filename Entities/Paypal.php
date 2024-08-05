a9ff1d3329b2254b25dbc50df011c7d3fe3d75f2
Dedan <dedanirungu@gmail.com>
2024-08-05 11:56:00 +0300

    Changes to files: Entities/Ipn.php Entities/Paypal.php database/migrations/2024_08_03_083231_create_paypal_ipn_table.php database/migrations/2024_08_03_083238_create_paypal_paypal_table.php

--- Entities/Paypal.php
+++ Entities/Paypal.php
@@ -22,29 +22,7 @@ class Paypal extends BaseModel
      */
     protected $table = "paypal";
 
-    /**
-     * List of fields to be migrated to the datebase when creating or updating model during migration.
-     *
-     * @param Blueprint $table
-     * @return void
-     */
-    public function fields(Blueprint $table = null): void
-    {
-        $this->fields = $table ?? new Blueprint($this->table);
-
-
-
-        $this->fields->increments('id')->html('hidden');
-        $this->fields->string('name')->html('text');
-        $this->fields->string('description')->html('textarea');
-        $this->fields->string('published')->html('switch');
-        $this->fields->string('payment_id')->nullable()->html('text');
-        $this->fields->string('transaction_code')->nullable()->html('text');
-        $this->fields->string('status')->nullable()->html('text');
-        $this->fields->string('amount')->nullable()->html('text');
-        $this->fields->string('currency')->nullable()->html('text');
-        $this->fields->string('item_number')->nullable()->html('text');
-    }
+ 
 
 
   
--- database/migrations/2024_08_03_083238_create_paypal_paypal_table.php
+++ database/migrations/2024_08_03_083238_create_paypal_paypal_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('paypal_paypal', function (Blueprint $table) {
             $table->id();
-            
+
+            $table->string('name');
+            $table->text('description');
+            $table->string('published');
+            $table->string('payment_id')->nullable();
+            $table->string('transaction_code')->nullable();
+            $table->string('status')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('item_number')->nullable();
+
             $table->timestamps();
         });
     }
