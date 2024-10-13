441c415fa4bef21c87b139657b7904deff12cd1f
Dedan <dedanirungu@gmail.com>
2024-10-13 16:36:33 +0300

    Release 2024.13

---
+++ app/Filament/Resources/IpnResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Paypal\Filament\Resources\IpnResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
---
+++ app/Filament/Resources/IpnResource/Pages/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Paypal\Filament\Resources\IpnResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\ListingBase;
+
+// Class List that extends ListBase
+class Listing extends ListingBase
+{
+}
