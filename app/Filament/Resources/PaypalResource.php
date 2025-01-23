34579652a13495ba182a90559f27841d5e0d5f87
Dedan <dedanirungu@gmail.com>
2025-01-23 03:42:09 +0300

    Make Filament Resource Shorter

--- app/Filament/Resources/PaypalResource.php
+++ app/Filament/Resources/PaypalResource.php
@@ -2,11 +2,7 @@
 
 namespace Modules\Paypal\Filament\Resources;
 
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Paypal\Filament\Resources\PaypalResource\Pages;
 use Modules\Paypal\Models\Paypal;
 
 class PaypalResource extends BaseResource
@@ -21,42 +17,4 @@ class PaypalResource extends BaseResource
 
     protected static ?string $navigationParentItem = 'Gateway';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
